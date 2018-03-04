e1bdf41245a8bffbb24d119295490d387f5deee1
Adrian Cann <adriantcann@gmail.com>
2018-03-04 08:57:37 -0500

    Add some seeds for local development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+vanguard_funds = [
+  {
+    ticker: "VTSMX",
+    name: "Vanguard Total Stock Market Index Fund Investor Shares",
+    description: "Vanguard Total Stock Market Index offers diversified exposure to U.S. stocks of all sizes."
+  },
+  {
+    ticker: "VBMFX",
+    name: "Vanguard Total Bond Market Index Fund Investor Shares",
+    description: "This fund is designed to provide broad exposure to U.S. investment grade bonds. Reflecting this goal, the fund invests about 30% in corporate bonds and 70% in U.S. government bonds of all maturities (short-, intermediate-, and long-term issues)."
+  },
+  {
+    ticker: "VGSIX",
+    name: "Vanguard Real Estate Index Fund Investor Shares",
+    description: "This fund invests in real estate investment trusts—companies that purchase office buildings, hotels, and other real estate property. REITs have often performed differently than stocks and bonds, so this fund may offer some diversification to a portfolio already made up of stocks and bonds."
+  }
+]
+
+vanguard_funds.each do |attributes|
+  Fund.create!(attributes)
+end
